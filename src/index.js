815dbfda69df32366218dde2060b4e7df59659dc
Brian Donnoe <dunow33@hotmail.com>
2017-11-21 17:42:35 -0500

    2nd friends list commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<div className="row">
+		<App>
+			<Header />
+		</App>
+	</div>, 
+	document.getElementById('root'));
 registerServiceWorker();
